9fcfa8e901aa83b06f5e5cf3cfc71702ff3e74c3
Luis Enco <encoluis@hotmail.com>
2024-05-30 18:01:43 -0500

    Copy some dot files from old msys2 user home

--- .chezmoiscripts/windows/run_once_before_01-install-msys2.ps1
+++ .chezmoiscripts/windows/run_once_before_01-install-msys2.ps1
@@ -4,11 +4,15 @@ $releaseInfo = Invoke-RestMethod -Uri $msys2Url -Headers @{ "User-Agent" = "Powe
 $asset = $releaseInfo.assets | Where-Object { $_.name -match "msys2-x86_64-[0-9]{8}\.exe$" } | Select-Object -First 1
 $assetUrl = $asset.browser_download_url
 
-$outputPath = "C:\Users\Warender\Downloads\msys2-x86_64-latest.exe"
-Invoke-WebRequest -Uri $assetUrl -OutFile $outputPath
+$msys2Installer = "$env:USERPROFILE\Downloads\msys2-x86_64-latest.exe"
+Invoke-WebRequest -Uri $assetUrl -OutFile $msys2Installer
+
+$msys2RootPath = "$env:USERPROFILE/Toolkit/msys64"
 
 Write-Host "Installing Msys2..."
-$installerArgs = "in --confirm-command --accept-messages --root C:/Users/Warender/Toolkit/msys64"
-Start-Process -FilePath $outputPath -ArgumentList $installerArgs -Wait
+$installerArgs = "in --confirm-command --accept-messages --root $msys2RootPath"
+Start-Process -FilePath $msys2Installer -ArgumentList $installerArgs -Wait
+Remove-Item $msys2Installer
 
-Remove-Item $outputPath
+Write-Host "Setting up some files before finishing..."
+Copy-Item -Path "$msys2RootPath\home\$env:USERNAME\.profile" -Destination "$env:USERPROFILE\" -Force
