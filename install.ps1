38f1a32c492a77e05c6b682df5b2c71c15dad002
Luis Enco <encoluis@hotmail.com>
2024-07-23 05:59:21 +0000

    add installer

---
+++ install.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+scoop install main/pwsh
+scoop install main/chezmoi
+
+chezmoi init --apply crealhex
